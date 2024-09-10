4811b84a3f85f4951ce0963c599205313b0f01f8
goodlandsecurity <josing.jj@gmail.com>
2024-09-10 16:13:21 -0500

    use object instead of updating attribute

--- misp_modules/modules/expansion/virustotal_pyoti_enrichment.py
+++ misp_modules/modules/expansion/virustotal_pyoti_enrichment.py
@@ -1,5 +1,5 @@
 import json
-from pymisp import MISPEvent, MISPAttribute
+from pymisp import MISPEvent, MISPObject
 from pyoti.multis import VirusTotalV3
 from . import check_input_attribute, checking_error, standard_error_message
 
@@ -23,10 +23,10 @@ moduleinfo = {
     'name': 'VT PyOTI Enrichment',
     'logo': 'virustotal.png',
     'requirements': ['Access to VirusTotal (apikey)'],
-    'features': "The module takes a hash attribute as input and queries VirusTotal's API to fetch additional data about it. The result, if the hash has a threat label tag the MISPAttribute with it. Also, check if the hash is from a known software distributor and tag MISPAttribute with PyOTI taxonomy tag.",
+    'features': "The module takes a hash attribute as input and queries VirusTotal's API to fetch additional data about it. The result, if the hash has a threat label tag the MISPAttribute with it. Also, check if the hash is from a known software distributor and tag MISPAttribute with PyOTI taxonomy tag and create a file object describing the file the input hash is related to.",
     'references': ['https://github.com/RH-ISAC/PyOTI', 'https://www.virustotal.com'],
     'input': 'A hash attribute (md5, sha1, sha256).',
-    'output': 'Tagging of the queried hash attribute.',
+    'output': 'File object related to the input attribute found on VirusTotal.',
 }
 moduleconfig = ["apikey"]
 
@@ -37,23 +37,41 @@ def run_enrichment(apikey: str, attribute: dict) -> dict:
     return vt.check_hash()
 
 
-def parse_response(response: dict, attribute: dict):
+def parse_response(response: dict):
+    attribute_mapping = {
+        'md5': {'type': 'md5', 'object_relation': 'environment', 'distribution': 5},
+        'sha1': {'type': 'sha1', 'object_relation': 'environment', 'distribution': 5},
+        'sha256': {'type': 'sha256', 'object_relation': 'environment', 'distribution': 5},
+        'imphash': {'type': 'imphash', 'object_relation': 'environment', 'distribution': 5},
+        'size': {'size-in-bytes': 'imphash', 'object_relation': 'environment', 'distribution': 5},
+        'meaningful_name': {'filename': 'imphash', 'object_relation': 'environment', 'distribution': 5}
+    }
     misp_event = MISPEvent()
-    misp_attribute = MISPAttribute()
-    misp_attribute.from_dict(**attribute)
+    misp_object = MISPObject('file')
+    for feature, attribute in attribute_mapping.items():
+        if feature in response.keys()  and response[feature]:
+            if feature in ('md5', 'sha1', 'sha256'):
+                misp_attribute = {
+                    'tags': [],
+                }
+                if response['data']['attributes'].get('popular_threat_classification'):
+                    threat_label = response['data']['attributes']['popular_threat_classification']['suggested_threat_label']
+                    misp_attribute['tags'].append(threat_label)
+                if response['data']['attributes'].get('known_distributors'):
+                    distributors = response['data']['attributes']['known_distributors']['distributors']
+                    misp_attribute['tags'].append(distributors)
+                    misp_attribute['comment'] = f'Distributors: {distributors}'
+                misp_attribute['value'] = response[feature]
+                misp_attribute.update(attribute)
+                misp_object.add_attribute(**misp_attribute)
+            else:
+                misp_attribute = {'value': response[feature]}
+                misp_attribute.update(attribute)
+                misp_object.add_attribute(**misp_attribute)
+    misp_event.add_object(**misp_object)
 
-    if response['data']['attributes'].get('popular_threat_classification'):
-        threat_label = response['data']['attributes']['popular_threat_classification']['suggested_threat_label']
-        misp_attribute.add_tag(threat_label)
-
-    if response['data']['attributes'].get('known_distributors'):
-        misp_attribute.add_tag('pyoti:virustotal="known-distributor"')
-        distributors = response['data']['attributes']['known_distributors']['distributors']
-        misp_attribute.comment = f'Distributors: {distributors}'
-
-    misp_event.add_attribute(**misp_attribute)
     event = json.loads(misp_event.to_json())
-    results = {'Attribute': event['Attribute']}
+    results = {'Object': event['Object']}
 
     return {'results': results}
 
@@ -79,7 +97,7 @@ def handler(q=False):
     vt_response = run_enrichment(request['config']['apikey'], attribute)
 
     if vt_response.get('data'):
-        return parse_response(vt_response, attribute)
+        return parse_response(vt_response)
 
     elif vt_response.get('error'):
         misperrors['error'] = vt_response['error']['message']
